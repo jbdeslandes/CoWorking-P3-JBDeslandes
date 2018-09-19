9aca922613650352d1282ace4729abe0c27f3e4e
JBDeslandes <jbdeslandes@hotmail.fr>
2018-09-19 15:05:22 +0200

    Game / Team / Character / Weapon files created

---
+++ CoWorking-P3-JBDeslandes/Character.swift
@@ -0,0 +1,9 @@
+//
+//  Character.swift
+//  CoWorking-P3-JBDeslandes
+//
+//  Created by Jean-Baptiste Deslandes on 19/09/2018.
+//  Copyright © 2018 Jean-Baptiste Deslandes. All rights reserved.
+//
+
+import Foundation

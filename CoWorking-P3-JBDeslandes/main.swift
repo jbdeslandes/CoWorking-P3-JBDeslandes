f013b5675e43fcba59e89c6993f05d0584b84cd7
JBDeslandes <jbdeslandes@hotmail.fr>
2018-09-20 20:46:51 +0200

    main file modified global variables added, Player creation added, Team creation added

--- CoWorking-P3-JBDeslandes/main.swift
+++ CoWorking-P3-JBDeslandes/main.swift
@@ -8,9 +8,26 @@
 
 import Foundation
 
+var character1: Character? = nil
+var character2: Character? = nil
+var character3: Character? = nil
+
+var currentTeam: Team
+var currentCharacter: Character
+
 print("Bienvenue dans l'arène !")
 print()
 
 let game = Game()
 
-game.createTeams()
+// PLAYERS CREATION
+
+var team1 = Team(name: "")
+var team2 = Team(name: "")
+
+game.createPlayers()
+
+// TEAMS CREATION
+
+print("\(team1.name) - Choisis le nom des héros qui composeront ton équipe !")
+print()

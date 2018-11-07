054dae5d8a66481ee5bf11857ed4b9d4f79e54be
JBDeslandes <jbdeslandes@hotmail.fr>
2018-11-07 13:08:48 +0100

    Constants file: - STICKDAMAGE modified - MACEDAMAGE added - DAGGERDAMAGE added - SPEARDAMAGE added

--- CoWorking-P3-JBDeslandes/Constants.swift
+++ CoWorking-P3-JBDeslandes/Constants.swift
@@ -23,11 +23,17 @@ let DEAD = 0
 // Balancing values:
 
 //let SWORDDAMAGE = 10
-//let STICKDAMAGE = 0
+//let STICKDAMAGE = Int.random(in: 5...15)
 //let FISTSDAMAGE = 8
 //let AXEDAMAGE = 12
+//let MACEDAMAGE = Int.random(in: 8...12)
+//let DAGGERDAMAGE = Int.random(in: 1...25)
+//let SPEARDAMAGE = Int.random(in: 5...15)
 
 let SWORDDAMAGE = 50
 let STICKDAMAGE = 50
 let FISTSDAMAGE = 50
 let AXEDAMAGE = 50
+let MACEDAMAGE = 50
+let DAGGERDAMAGE = 50
+let SPEARDAMAGE = 50

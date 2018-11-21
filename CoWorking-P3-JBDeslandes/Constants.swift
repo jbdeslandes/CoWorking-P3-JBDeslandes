924fc6312c6d9451232b87b223dda0554c7d1600
JBDeslandes <jbdeslandes@hotmail.fr>
2018-11-21 13:01:32 +0100

    Constants file: - let TEAMNUMBER added - min / max damages for Weapons added

--- CoWorking-P3-JBDeslandes/Constants.swift
+++ CoWorking-P3-JBDeslandes/Constants.swift
@@ -4,6 +4,10 @@
 
 // Team:
 
+//let TEAMNUMBER = 2
+
+let TEAMNUMBER = 2
+
 //let CHARACTERNUMBER = 3
 
 let CHARACTERNUMBER = 3
@@ -29,17 +33,36 @@ let DEAD = 0
 // Balancing values:
 
 //let SWORDDAMAGE = 10
-//let STICKDAMAGE = Int.random(in: 5...15)
+//let STICKDAMAGEMIN = 5
+//let STICKDAMAGEMAX = 15
 //let FISTSDAMAGE = 8
 //let AXEDAMAGE = 12
-//let MACEDAMAGE = Int.random(in: 8...12)
-//let DAGGERDAMAGE = Int.random(in: 1...25)
-//let SPEARDAMAGE = Int.random(in: 5...15)
+//let MACEDAMAGEMIN = 8
+//let MACEDAMAGEMAX = 12
+//let DAGGERDAMAGEMIN = 1
+//let DAGGERDAMAGEMAX = 25
+//let SPEARDAMAGEMIN = 5
+//let SPEARDAMAGEMAX = 15
 
-let SWORDDAMAGE = 50
-let STICKDAMAGE = 50
-let FISTSDAMAGE = 50
-let AXEDAMAGE = 50
-let MACEDAMAGE = 50
-let DAGGERDAMAGE = 50
-let SPEARDAMAGE = 50
+let SWORDDAMAGE = 10
+let STICKDAMAGEMIN = 5
+let STICKDAMAGEMAX = 15
+var STICKDAMAGE = 0
+let FISTSDAMAGE = 8
+let AXEDAMAGE = 12
+let MACEDAMAGEMIN = 8
+let MACEDAMAGEMAX = 12
+let MACEDAMAGE = 0
+let DAGGERDAMAGEMIN = 1
+let DAGGERDAMAGEMAX = 25
+var DAGGERDAMAGE = 0
+let SPEARDAMAGEMIN = 5
+let SPEARDAMAGEMAX = 15
+var SPEARDAMAGE = 0
+
+/*
+ I - COMPLETER :
+ - probleme heal dépasse maxlife
+ - Les dégats des armes(random) ne changent pas de chiffre sur la plage de dégats.
+ - Bug sur deadTeam() / var alive
+ */

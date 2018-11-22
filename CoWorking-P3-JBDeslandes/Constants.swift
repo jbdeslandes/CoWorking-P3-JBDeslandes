87fa22001c1b43da88ed22cb69df7049c49b5440
JBDeslandes <jbdeslandes@hotmail.fr>
2018-11-22 22:19:31 +0100

    Constants file: - STICKDAMAGE + MACEDAMAGE + DAGGERDAMAGE + SPEARDAMAGE erased

--- CoWorking-P3-JBDeslandes/Constants.swift
+++ CoWorking-P3-JBDeslandes/Constants.swift
@@ -2,8 +2,6 @@
 // MARK: - Characters
 //====================
 
-// Team:
-
 //let TEAMNUMBER = 2
 
 let TEAMNUMBER = 2
@@ -11,8 +9,6 @@ let TEAMNUMBER = 2
 //let CHARACTERNUMBER = 3
 
 let CHARACTERNUMBER = 3
-
-// Balancing values:
 
 //let FIGHTERLIFE = 100
 //let WIZARDLIFE = 100
@@ -47,22 +43,11 @@ let DEAD = 0
 let SWORDDAMAGE = 10
 let STICKDAMAGEMIN = 5
 let STICKDAMAGEMAX = 15
-var STICKDAMAGE = 0
 let FISTSDAMAGE = 8
 let AXEDAMAGE = 12
 let MACEDAMAGEMIN = 8
 let MACEDAMAGEMAX = 12
-let MACEDAMAGE = 0
 let DAGGERDAMAGEMIN = 1
 let DAGGERDAMAGEMAX = 25
-var DAGGERDAMAGE = 0
 let SPEARDAMAGEMIN = 5
 let SPEARDAMAGEMAX = 15
-var SPEARDAMAGE = 0
-
-/*
- I - COMPLETER :
- - probleme heal dépasse maxlife
- - Les dégats des armes(random) ne changent pas de chiffre sur la plage de dégats.
- - Bug sur deadTeam() / var alive
- */
